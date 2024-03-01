1682701f94795a4267c9012bafbe098b363d0df8
dmutro <murchyk2007@gmail.com>
2024-03-01 20:52:56 +0200

    lesson 01.03.2024

--- db_scripts.py
+++ db_scripts.py
@@ -1,13 +1,16 @@
 import sqlite3
 
 db_name = "blog.db"
-connectioin = None
+connection = None
 cursor = None
 
 def open():
     global connectioin,cursor
     connectioin = sqlite3.connect(db_name)
     cursor = connectioin.cursor()
+
+def new_func():
+    return db_name
 
 def close():
     cursor.close()
@@ -19,3 +22,30 @@ def getUser():
     user = cursor.fetchone()
     close()
     return user
+
+def getIdByCategory(category_name):
+    open()
+    cursor.execute(''' SELECT  category_id
+                       FROM category
+                       WHERE category_name = (?) 
+                       ''', [category_name])
+    category_id = cursor.fetchone()[0]
+    return category_id
+
+def getPostsByCategory(category_name):
+    open()
+    cursor.execute(''' SELECT * FROM post, category
+                       WHERE category_name = (?)
+                       AND post.category_id = category.category_id
+                       ORDER BY post_datetime DESC 
+                       ''', [category_name])
+    posts = cursor.fetchall()
+    close()
+    return posts
+
+def addPost(category_id, post_text):
+    open()
+    cursor.execute('''INSERT INTO post (category_id, post_text)
+                      VALUES ((?), (?)))''', [category_id, post_text])
+    connection.commit()
+    close()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 import settings as stg
 import db_scripts as db
 
@@ -8,11 +8,20 @@ app.config["SECRET_KEY"] = stg.SECRET_KEY
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    return render_template("index.html") 
 
-@app.route("/post/category")
-def post_category():
-    return "ТУТ БУДУТЬ КАТЕГОРІЇ ПОСТІВ"
+@app.route("/post/category/<category_name>")
+def post_category(category_name):
+    category_id = db.getIdByCategory(category_name) 
+
+    if request.method == "GET":
+        posts = db.getPostsByCategory(category_name)
+
+    return render_template("post_category.html", category_name=category_name, category_id=category_id, posts=posts)
+
+    
+
+
 
 @app.route("/post/view")
 def post_view():
